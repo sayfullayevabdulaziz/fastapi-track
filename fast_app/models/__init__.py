f847fb0a84974064ad550407aa377b0e0667b521
sayfullayevabdulaziz <90500596+sayfullayevabdulaziz@users.noreply.github.com>
2023-02-25 00:44:35 +0500

    delete fast-app

--- fast_app/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from .role_model import Role
-from .track_model import Track
-from .user_model import User
-from .image_model import ImageMedia
-from .blog_model import Blog
-from .comment_model import Comment
--- fast_app/utils/exceptions/__init__.py
+++
@@ -1,7 +0,0 @@
-from .common_exceptions import (
-    ContentNoChangeException,
-    IdNotFoundException,
-    NameExistException,
-    NameNotFoundException,
-)
-from .user_exceptions import UserSelfDeleteException
